3620fa7441d96d47e78c36a5a915d021d8119c7c
chetanay batra <chetanaybatta361@gmail.com>
2026-09-02 17:02:03 +0530

    Delete React-02-ProfileForge directory

--- React-02-ProfileForge/src/main.jsx
+++
@@ -1,8 +0,0 @@
-// Developer: Chetan Ay Builder
-// Project: React-01-Money-Calculator
-
-import {createRoot} from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css"
-
-createRoot(document.getElementById('root')).render(<App />);
