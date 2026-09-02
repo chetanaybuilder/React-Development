b2bf964dd77153ddfbac7c01da11de8135eb98f5
chetanaybuilder <chetanaybuilder@gmail.com>
2026-09-02 16:20:31 +0530

    Add React-02-ProfileForge project

---
+++ React-02-ProfileForge/src/components/header.jsx
@@ -0,0 +1,12 @@
+// Developer: Chetan Ay Builder
+// Project: React-01-Money-Calculator
+
+function Header() {
+    return (
+        <header>
+            <h1>Money Calculator</h1>
+            <p>Track your expenses and income with ease!</p>
+        </header>
+    );
+}
+export default Header;
---
+++ React-02-ProfileForge/src/main.jsx
@@ -0,0 +1,8 @@
+// Developer: Chetan Ay Builder
+// Project: React-01-Money-Calculator
+
+import {createRoot} from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css"
+
+createRoot(document.getElementById('root')).render(<App />);
